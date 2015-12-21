1f0747c2009b89db53a2c87dc325370c3df30523
geansru <d@geans.ru>
2015-12-21 18:02:12 +0400

    Complete tutorial

--- horizontal-paging-sample/ViewController.swift
+++ horizontal-paging-sample/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: @IBOutlet's
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var clickButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //1
+        self.scrollView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
+        let scrollViewWidth:CGFloat = self.scrollView.frame.width
+        let scrollViewHeight:CGFloat = self.scrollView.frame.height
+        //2
+        textView.textAlignment = .Center
+        textView.text = "Sweettutos.com is your blog of choice for Mobile tutorials"
+        textView.textColor = .blackColor()
+        clickButton.layer.cornerRadius = 4.0
+        //3
+        let imgOne = UIImageView(frame: CGRectMake(0, 0,scrollViewWidth, scrollViewHeight))
+        imgOne.image = UIImage(named: "Slide 1")
+        let imgTwo = UIImageView(frame: CGRectMake(scrollViewWidth, 0,scrollViewWidth, scrollViewHeight))
+        imgTwo.image = UIImage(named: "Slide 2")
+        let imgThree = UIImageView(frame: CGRectMake(scrollViewWidth*2, 0,scrollViewWidth, scrollViewHeight))
+        imgThree.image = UIImage(named: "Slide 3")
+        let imgFour = UIImageView(frame: CGRectMake(scrollViewWidth*3, 0,scrollViewWidth, scrollViewHeight))
+        imgFour.image = UIImage(named: "Slide 4")
+        
+        self.scrollView.addSubview(imgOne)
+        self.scrollView.addSubview(imgTwo)
+        self.scrollView.addSubview(imgThree)
+        self.scrollView.addSubview(imgFour)
+        //4
+        self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.width * 4, self.scrollView.frame.height)
+        self.scrollView.delegate = self
+        self.pageControl.currentPage = 0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        // Test the offset and calculate the current page after scrolling ends
+        let pageWidth:CGFloat = CGRectGetWidth(scrollView.frame)
+        let currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
+        // Change the indicator
+        self.pageControl.currentPage = Int(currentPage);
+        // Change the text accordingly
+        if Int(currentPage) == 0{
+            textView.text = "Sweettutos.com is your blog of choice for Mobile tutorials"
+        }else if Int(currentPage) == 1{
+            textView.text = "I write mobile tutorials mainly targeting iOS"
+        }else if Int(currentPage) == 2{
+            textView.text = "And sometimes I write games tutorials about Unity"
+        }else{
+            textView.text = "Keep visiting sweettutos.com for new coming tutorials, and don't forget to subscribe to be notified by email :)"
+            // Show the "Let's Start" button in the last slide (with a fade in animation)
+            UIView.animateWithDuration(1.0, animations: { () -> Void in
+                self.clickButton.alpha = 1.0
+            })
+        }
+    }
+}
